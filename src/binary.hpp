f462601445ad7c5c4fa1765b1ade08d4c2e30cb5
贺鹏飞 <hepengfei@xunlei.com>
2016-10-10 10:43:01 +0800

    添加binary::Decoder实现

--- src/binary.hpp
+++ src/binary.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>
+
+#include <string>
 
 namespace kb {
   namespace binary {
@@ -140,12 +143,178 @@ namespace kb {
       d[7] = value;
     }
 
-    struct Encoder {
-      
+    enum {
+      E_NIL = 0,
+      E_OVERFLOW,
+      E_BAD_VARINT,
     };
+
+    template <typename Endian>
     struct Decoder {
-      
+      Decoder(const void *data, size_t len)
+        : _data((const uint8_t*)data), _len(len), _offset(0), _error(0) {}
+
+      uint8_t Uint8();
+      uint16_t Uint16();
+      uint32_t Uint32();
+      uint64_t Uint64();
+
+      int8_t Int8() { return int8_t(Uint8()); }
+      int16_t Int16() { return int16_t(Uint16()); }
+      int32_t Int32() { return int32_t(Uint32()); }
+      int64_t Int64() { return int64_t(Uint64()); }
+
+      uint32_t Varint32Rtmfp();
+      uint64_t Varint64Rtmfp();
+
+      const uint8_t *Data(size_t len);
+
+      std::string String(size_t len) {
+        const char *data = (const char*)Data(len);
+        if (data == NULL) {
+          return std::string();
+        }
+        return std::string(data, len);
+      }
+
+      std::string String() { return String(Uint32()); }
+
+      size_t Used() const { return _offset; }
+      size_t Left() const { return _len - _offset; }
+      void Skip(size_t bytes) { _offset += bytes; }
+      int Error() const { return _error; }
+
+    protected:
+      void setError(int error) { _error = error; }
+
+    private:
+      const uint8_t *_data;
+      size_t _len;
+      size_t _offset;
+      int _error;
     };
+
+    template <typename Endian>
+    inline
+    uint8_t Decoder<Endian>::Uint8() {
+      if (Error()) {
+        return 0;
+      }
+      if (Left() < 1) {
+        setError(E_OVERFLOW);
+        return 0;
+      }
+
+      uint8_t value = *(_data + _offset);
+      Skip(1);
+      return value;
+    }
+
+    template <typename Endian>
+    inline
+    uint16_t Decoder<Endian>::Uint16() {
+      if (Error()) {
+        return 0;
+      }
+      if (Left() < 2) {
+        setError(E_OVERFLOW);
+        return 0;
+      }
+
+      uint16_t value = Endian::Uint16(_data + _offset);
+      Skip(2);
+      return value;
+    }
+
+    template <typename Endian>
+    inline
+    uint32_t Decoder<Endian>::Uint32() {
+      if (Error()) {
+        return 0;
+      }
+      if (Left() < 4) {
+        setError(E_OVERFLOW);
+        return 0;
+      }
+
+      uint32_t value = Endian::Uint32(_data + _offset);
+      Skip(4);
+      return value;
+    }
+
+    template <typename Endian>
+    inline
+    uint64_t Decoder<Endian>::Uint64() {
+      if (Error()) {
+        return 0;
+      }
+      if (Left() < 8) {
+        setError(E_OVERFLOW);
+        return 0;
+      }
+
+      uint64_t value = Endian::Uint64(_data + _offset);
+      Skip(8);
+      return value;
+    }
+
+    template <typename Endian>
+    inline
+    const uint8_t *Decoder<Endian>::Data(size_t len) {
+      if (Error()) {
+        return NULL;
+      }
+      if (Left() < len) {
+        setError(E_OVERFLOW);
+        return NULL;
+      }
+      const uint8_t *value = _data + _offset;
+      Skip(len);
+      return value;
+    }
+
+    template <typename Endian>
+    inline
+    uint32_t Decoder<Endian>::Varint32Rtmfp() {
+      uint32_t value = 0;
+      const int maxBytes = 4;
+      for (int i = 0; i < maxBytes && !Error(); ++i) {
+        uint8_t bit7 = Uint8();
+        value = (value << 7) | (bit7 & 0x7F);
+
+        if ((bit7 & 0x80) == 0) {
+          return value;
+        }
+      }
+
+      if (!Error()) {
+        setError(E_BAD_VARINT);
+      }
+
+      return 0;
+    }
+
+    template <typename Endian>
+    inline
+    uint64_t Decoder<Endian>::Varint64Rtmfp() {
+      uint64_t value = 0;
+      const int maxBytes = 9;
+      for (int i = 0; i < maxBytes && !Error(); ++i) {
+        uint8_t bit7 = Uint8();
+        value = (value << 7) | (bit7 & 0x7F);
+
+        if ((bit7 & 0x80) == 0) {
+          return value;
+        }
+      }
+
+      if (!Error()) {
+        setError(E_BAD_VARINT);
+      }
+
+      return 0;
+    }
+
   }
 
 
--- tests/binary_test.cpp
+++ tests/binary_test.cpp
@@ -59,3 +59,103 @@ TEST(Binary, Uint64) {
   kb::binary::BigEndian::PutUint64(buffer, valueBigEndian);
   ASSERT_THAT(buffer, testing::ElementsAreArray(data));
 }
+
+TEST(Decoder, Integers) {
+  const unsigned char data[] = { 0x12, 0x34, 0x56, 0x78,
+                                 0x9a, 0xbc, 0xde, 0xf0 };
+  const size_t len = sizeof(data);
+
+  typedef kb::binary::Decoder<kb::binary::LittleEndian> LittleEndianDecoder;
+
+  LittleEndianDecoder decoder1(data, len);
+  for (size_t i = 0; i < len; ++i) {
+    ASSERT_THAT(decoder1.Uint8(), testing::Eq(data[i]));
+  }
+
+  LittleEndianDecoder decoder2(data, len);
+  ASSERT_THAT(decoder2.Uint16(), testing::Eq(0x3412));
+  ASSERT_THAT(decoder2.Uint16(), testing::Eq(0x7856));
+  ASSERT_THAT(decoder2.Uint16(), testing::Eq(0xbc9a));
+  ASSERT_THAT(decoder2.Uint16(), testing::Eq(0xf0de));
+  ASSERT_THAT(decoder2.Uint16(), testing::Eq(0x0));
+}
+
+TEST(Decoder, String) {
+  const unsigned char data[] = { 0x5, 0x0, 0x0, 0x0,
+                                 'H', 'e', 'l', 'l', 'o' };
+  const size_t len = sizeof(data);
+
+  typedef kb::binary::Decoder<kb::binary::LittleEndian> LittleEndianDecoder;
+
+  LittleEndianDecoder decoder1(data, len);
+  std::string str1 = decoder1.String();
+  ASSERT_THAT(str1, testing::Eq("Hello"));
+
+  LittleEndianDecoder decoder2(data, len);
+  uint32_t len2 = decoder2.Uint32();
+  ASSERT_THAT(len2, testing::Eq(5));
+  std::string str2 = decoder2.String(len2);
+  ASSERT_THAT(str2, testing::Eq("Hello"));
+}
+
+TEST(Decoder, Varint32Rtmfp) {
+  const unsigned char data[] = {
+    0x5,
+    0x85, 0x5,
+    0x85, 0x85, 0x5,
+    0x85, 0x85, 0x85, 0x5,
+    0x85, 0x85, 0x85, 0x85, 0x5 // bad data
+  };
+  const size_t len = sizeof(data);
+  const uint32_t value[] = {
+    0x5,
+    (0x5 << 7 | 0x5),
+    (0x5 << 14 | 0x5 << 7 | 0x5),
+    (0x5 << 21 | 0x5 << 14 | 0x5 << 7 | 0x5),
+    0                           // for bad data
+  };
+
+  typedef kb::binary::Decoder<kb::binary::LittleEndian> LittleEndianDecoder;
+
+  LittleEndianDecoder decoder1(data, len);
+  for (size_t i = 0; i < sizeof(value) / sizeof(value[0]); ++i) {
+    ASSERT_THAT(decoder1.Varint32Rtmfp(), value[i]);
+  }
+  ASSERT_THAT(decoder1.Error(), testing::Eq(kb::binary::E_BAD_VARINT));
+}
+
+TEST(Decoder, Varint64Rtmfp) {
+  const unsigned char data[] = {
+    0x5,
+    0x85, 0x5,
+    0x85, 0x85, 0x5,
+    0x85, 0x85, 0x85, 0x5,
+    0x85, 0x85, 0x85, 0x85, 0x5,
+    0x85, 0x85, 0x85, 0x85, 0x85, 0x5,
+    0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x5,
+    0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x5,
+    0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x5,
+    0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x85, 0x5, // bad data
+  };
+  const size_t len = sizeof(data);
+  const uint64_t value[] = {
+    0x5ULL,
+    (0x5ULL << 7 | 0x5ULL),
+    (0x5ULL << 14 | 0x5ULL << 7 | 0x5ULL),
+    (0x5ULL << 21 | 0x5ULL << 14 | 0x5ULL << 7 | 0x5ULL),
+    (0x5ULL << 28 | 0x5ULL << 21 | 0x5ULL << 14 | 0x5ULL << 7 | 0x5ULL),
+    (0x5ULL << 35 | 0x5ULL << 28 | 0x5ULL << 21 | 0x5ULL << 14 | 0x5ULL << 7 | 0x5ULL),
+    (0x5ULL << 42 | 0x5ULL << 35 | 0x5ULL << 28 | 0x5ULL << 21 | 0x5ULL << 14 | 0x5ULL << 7 | 0x5ULL),
+    (0x5ULL << 49 | 0x5ULL << 42 | 0x5ULL << 35 | 0x5ULL << 28 | 0x5ULL << 21 | 0x5ULL << 14 | 0x5ULL << 7 | 0x5ULL),
+    (0x5ULL << 56 | 0x5ULL << 49 | 0x5ULL << 42 | 0x5ULL << 35 | 0x5ULL << 28 | 0x5ULL << 21 | 0x5ULL << 14 | 0x5ULL << 7 | 0x5ULL),
+    0                           // for bad data
+  };
+
+  typedef kb::binary::Decoder<kb::binary::LittleEndian> LittleEndianDecoder;
+
+  LittleEndianDecoder decoder1(data, len);
+  for (size_t i = 0; i < sizeof(value) / sizeof(value[0]); ++i) {
+    ASSERT_THAT(decoder1.Varint64Rtmfp(), value[i]) << "i = " << i;
+  }
+  ASSERT_THAT(decoder1.Error(), testing::Eq(kb::binary::E_BAD_VARINT));
+}
